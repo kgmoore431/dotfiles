deb4f1e1c740823b6dd758057bd6ec06a49cca05
Kevin Moore <kevin@grandrounds.com>
2017-06-15 23:18:08 -0700

    autolink sublime prefs to this dir

--- sublime3/link_subl_prefs.sh
+++ sublime3/link_subl_prefs.sh
@@ -3,12 +3,19 @@ set -e
 # Backup the Current prefs for sublime 3 and re-link the user preferences to this repo
 
 subl_settings_path="${HOME}/Library/Application Support/Sublime Text 3/Packages/"
+my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+# pushd "${subl_settings_path}" >/dev/null  2>&1
 
 if [[ -d "${subl_settings_path}/User" ]]; then
-    tar -cHvzf "sublime3_user_settings_$(date +%F).backup.tgz" -C "${subl_settings_path}" User/
+    backup_file="sublime3_user_settings_$(date +%F).backup.tgz"
+    echo "Backing Up current prefs as: ${backup_file}"
 
-
-
-else
-    echo "No existing user settings"
+    tar -cHzf "${backup_file}" -C "${subl_settings_path}" User/
 fi
+
+ln -Fis "$my_dir/Packages/User" "${subl_settings_path}"
+
+
+# popd >/dev/null 2>&1
