f97f94daec077415c7b8ba5a8e7573f38c799b8e
Kevin Moore <kevin@aurora.tech>
2017-11-27 21:25:50 -0800

    add homebrew dir to over-ride system python with brew python2

--- profile.d/01-paths.sh
+++ profile.d/01-paths.sh
@@ -10,5 +10,6 @@ function suffix_path { # add a suffix to the path if it exists and isn't already
 
 #Add some default locations to the exec path
 prefix_path "/usr/local/bin"
+prefix_path "/usr/local/opt/python/libexec/bin"
 prefix_path "${HOME}/bin"
 # prefix_path "${CORP_HOME}/ops-tools/bin"
