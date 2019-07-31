c22a1594e0d33aa5aa73b0c13385f2ce317ccc83
Kevin Moore <kevin@aurora.tech>
2019-07-30 20:50:34 -0700

    alias updates and sublime settings

--- profile.d/01-aliases.sh
+++ profile.d/01-aliases.sh
@@ -7,14 +7,31 @@ alias resource='source ~/.bash_profile'
 alias ops='cd ${CORP_HOME}/it-ops'
 alias web='cd ${CORP_HOME}/aurora-web'
 
-#web = test.aurora-staging.com
-alias stage_test='gsutil -m -h "Cache-Control:public,max-age=60" rsync  -x "\.DS_Store$|.*/\.DS_Store$" -c -d -R /Users/kmoore/src/aurora/aurora-web/published gs://test.aurora-staging.com/; echo "Pushed to: https://test.aurora-staging.com"'
+function gamfiles () {
+     gam user "${1}" show filelist todrive id alternatelink name owners shared
+}
 
-#test = web.aurora-staging.com
-alias stage_web='gsutil -m  -h "Cache-Control:public,max-age=60" rsync -x "\.DS_Store$|.*/\.DS_Store$" -c -d -R /Users/kmoore/src/aurora/aurora-web/published/ gs://aurora-staging-site/ ; echo "Pushed to: https://web.aurora-staging.com"'
 
-#Staging to Prod
-alias prod_web='gsutil -m -h "Cache-Control:public,max-age=86400" rsync  -x "robots.txt" -c -d -R gs://aurora-staging-site/ gs://aurora-tech-site/; ; echo "Pushed to: https://aurora.tech"'
+function gamtermdate () {
+    gam info user "${1}" |grep "Last login" |cut -f2 -d: |cut -f1 -d"T"
+}
+
+
+function find_receipt () {
+    amt="$1"
+    the_receipt="$(grep -r --include="*.html" ${amt} *|head -1 |cut -f1 -d:)"
+    receipt_dir="$(dirname "${the_receipt}")"
+    open "${receipt_dir}"
+}
+
+# #web = test.aurora-staging.com
+# alias stage_test='gsutil -m -h "Cache-Control:public,max-age=60" rsync  -x "\.DS_Store$|.*/\.DS_Store$" -c -d -R /Users/kmoore/src/aurora/aurora-web/published gs://test.aurora-staging.com/; echo "Pushed to: https://test.aurora-staging.com"'
+
+# #test = web.aurora-staging.com
+# alias stage_web='gsutil -m  -h "Cache-Control:public,max-age=60" rsync -x "\.DS_Store$|.*/\.DS_Store$" -c -d -R /Users/kmoore/src/aurora/aurora-web/published/ gs://aurora-staging-site/ ; echo "Pushed to: https://web.aurora-staging.com"'
+
+# #Staging to Prod
+# alias prod_web='gsutil -m -h "Cache-Control:public,max-age=86400" rsync  -x "robots.txt" -c -d -R gs://aurora-staging-site/ gs://aurora-tech-site/; ; echo "Pushed to: https://aurora.tech"'
 
 alias tf='terraform'
 alias mydrive='cd "/Volumes/GoogleDrive/My Drive"'
@@ -23,6 +40,7 @@ alias headshots='cd "/Volumes/GoogleDrive/My Drive/headshots@aurora.tech"'
 
 alias speed_check='speedtest --server 1749'
 alias chromecode='gam user chrome show backupcodes'
+alias asset_tag='echo "TBD-$(uuidgen |cut -f1 -d-)" |pbcopy'
 
 lcase_inits="$(tr [:upper:] [:lower:] <<< ${INITIALS})"
 
